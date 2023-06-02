6aa4ba0cc1dc6b48831734d501ada81d481dcf21
Sushil Kumar <skumar18@wayfair.com>
2023-06-02 18:25:41 +0530

    Logging received delay message

--- src/main/java/com/keda/kedademoconsumer/KafkaConsumer.java
+++ src/main/java/com/keda/kedademoconsumer/KafkaConsumer.java
@@ -11,6 +11,7 @@ public class KafkaConsumer {
   @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.consumer-group-id}")
   public void handle(String message) throws InterruptedException {
     int delay = Integer.parseInt(message);
+    System.out.println("Sleeping for : " + delay + " ms.");
     Thread.sleep(delay);
   }
 
